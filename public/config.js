181d2efc989e471fc57ef61bfea81d9b368763df
bclbusiness <bclbusinessprojects@gmail.com>
2026-05-09 07:15:07 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-const BAREEL_CONFIG = {
+window.BAREEL_CONFIG = {
   supabaseUrl: 'https://usrvxlmmepkuhneubxbm.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzcnZ4bG1tZXBrdWhuZXVieGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNjIwMjgsImV4cCI6MjA5MzYzODAyOH0.x6bzlk_PIIq9jYsjmmuKblMlC2iK-scjAlHLZwv0tRE',
 };
